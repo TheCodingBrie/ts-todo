5feec421baedf0ece65fe50884c12e82dd001fb9
TheCodingBrie <gbelangerbrie@gmail.com>
2022-07-21 16:18:12 +0200

    Finish project

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -1,4 +1,8 @@
+import { useEffect, useRef, useState } from "react";
 import { Todo } from "../model";
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+import { MdDone } from "react-icons/md";
+import TodoRecucer from "../reducer";
 
 type Props = {
   todo: Todo;
@@ -7,7 +11,73 @@ type Props = {
 };
 
 const TodoCard = ({ todo, todos, setTodos }: Props) => {
-  return <div></div>;
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.todo);
+
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+
+    console.log(todos);
+    setTodos(
+      todos.map((todo) => (todo.id === id ? { ...todo, todo: editTodo } : todo))
+    );
+
+    setEdit(false);
+  };
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [edit]);
+
+  return (
+    <form className="todo__card" onSubmit={(e) => handleEdit(e, todo.id)}>
+      {edit ? (
+        <input
+          ref={inputRef}
+          value={editTodo}
+          onChange={(e) => setEditTodo(e.target.value)}
+          className="todo__card--text"
+        />
+      ) : todo.isDone ? (
+        <s className="todo__card--text">{todo.todo}</s>
+      ) : (
+        <span className="todo__card--text">{todo.todo}</span>
+      )}
+
+      <div>
+        <span
+          className="icon"
+          onClick={() => {
+            if (!edit && !todo.isDone) {
+              setEdit(!edit);
+            }
+          }}
+        >
+          <AiFillEdit />
+        </span>
+        <span className="icon" onClick={() => handleDelete(todo.id)}>
+          <AiFillDelete />
+        </span>
+        <span className="icon" onClick={() => handleDone(todo.id)}>
+          <MdDone />
+        </span>
+      </div>
+    </form>
+  );
 };
 
 export default TodoCard;
